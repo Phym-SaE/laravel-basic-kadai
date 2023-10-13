0129bf86868bcd8d3fcabda6789252c5d8c3d2be
phym <135787491+Phym-SaE@users.noreply.github.com>
2023-10-13 16:58:27 +0900

    laravel-basic-kadai update

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Laravel基礎</title>
+    </head>
+
+    <body>
+        <p>Hello, world!</p>
+        <p>これは、index.blade.phpファイルです。</p>
+        <p>こんにちは、{{ $name }}さん！</p>
+        <ul>
+            @foreach ($languages as $language)
+                <li>{{ $language }}</li>
+            @endforeach
+        </ul>
+    </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [HelloController::class, 'index']);
+
+Route::get('/posts', [PostController::class, 'index']);
